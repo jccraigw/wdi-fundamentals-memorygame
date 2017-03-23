826a8a59680b648cc85816388232d13ebbf9ab8e
Julian Craigwell-Graham <julian@Julians-MacBook-Pro.local>
2017-03-22 20:40:44 -0400

    created dynamic HTML

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,11 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
 var cardOne= "queen";
 var cardTwo= "king";
 var cardThree= "queen";
 var cardFour= "king";
 
-if(cardTwo === cardFour){
+/*if(cardTwo === cardFour){
 	alert('You found a match!');
 }
 else if(cardOne === cardThree){
@@ -15,4 +15,34 @@ else if(cardOne === cardThree){
 else{
 
 	alert('Sorry, try again.');
-}
+}*/
+
+
+
+var createCards = function(){
+
+	var gameBoard = document.getElementById('game-board');
+
+		for(var i = 0; i < 4; i++){
+
+			var cards = document.createElement('div');
+
+			cards.className = 'card';
+
+			gameBoard.appendChild(cards);
+
+		}
+
+
+
+
+};
+
+createCards();
+
+
+
+
+
+
+
